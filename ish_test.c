50c841316e1249c82beded32eac775dff40c630a
bosskab123 <phatarapran7524@gmail.com>
2021-06-12 15:27:30 +0700

    Update ish_test.c

--- ish_test.c
+++ ish_test.c
@@ -198,8 +198,9 @@ int main(void)
 				DynArray_free(tokens);
 				
 				// Create a process to handle with the program.			
-				int child_status;	
-				child_status = execvp((char *)argv[0],(char **)argv);
+				int child_status;
+				if (number_token == 1) child_status = execvp((char *)argv[0],NULL);
+				else child_status = execvp((char *)argv[0],(char **)argv);
 				fprintf(stderr,"error status: %d for running %s\n",child_status, command);
 				exit(0);
 				
